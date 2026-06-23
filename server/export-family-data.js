87d7515ae61be1f1ae8f02dd1b9c3ef4fef82d95
godjiyoon@naver.com <godjiyoon@naver.com>
2026-06-23 22:57:28 +0900

    Noindex Dranvi Family pages

--- server/export-family-data.js
+++ server/export-family-data.js
@@ -14,6 +14,7 @@ function routeTemplate(slug) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow, noarchive">
     <title>DRANVI FAMILY No.${titleSlug}</title>
     <link rel="stylesheet" href="../../family-os.css">
 </head>
